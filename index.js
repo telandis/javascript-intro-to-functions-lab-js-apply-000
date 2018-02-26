cc6b113410a783cbe3647794bb884a348451e846
Victror Zhu <victorzhu123@live.com>
2018-02-26 19:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper(string) {
 function logShout(string) {
   console.log(shout(string));
 }
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  if (string === "I love you, Grandma.") {
+    return
+  }
+}
